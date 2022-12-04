c997f5b4c90cd85ff0c2e3d04037b48903bc3405
Utk <personal.utkarsh24@gmail.com>
2022-12-04 00:27:16 +0000

    chore : back to og

--- echo-python/echo.py
+++ echo-python/echo.py
@@ -13,7 +13,6 @@
 from os import environ
 import logging
 import requests
-import json
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
@@ -21,36 +20,11 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
-def hex2str(hex):
-    """
-    Decodes a hex string into a regular string
-    """
-    return bytes.fromhex(hex[2:]).decode("utf-8")
-
-def str2hex(str):
-    """
-    Encodes a string as a hex string
-    """
-    return "0x" + str.encode("utf-8").hex()
-
 def handle_advance(data):
-    input = hex2str(data["payload"])
-    logger.info(f"Received input: {input}")
-
-    jsonFormatted = json.loads(input)
-    output = "Unable to process data"
-
-    if jsonFormatted["accident"] & jsonFormatted["driverError"]:
-        output = "claim not processed"
-
-    logger.info(f"Adding notice with payload: '{output}'")
-    
-
-    # logger.info(f"Adding notice as {data}")
-    # output = data;
-    logger.info(f"Adding notice with payload: '{output}'")
-    
-    response = requests.post(rollup_server + "/notice", json={"payload": str2hex(str(output))})
+    logger.info(f"Received advance request data {data}")
+    logger.info("Adding notice")
+    notice = {"payload": data["payload"]}
+    response = requests.post(rollup_server + "/notice", json=notice)
     logger.info(f"Received notice status {response.status_code} body {response.content}")
     return "accept"
 
