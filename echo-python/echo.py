c58c555ee0502b5fedfd0906343ed9459c0def55
Utk <personal.utkarsh24@gmail.com>
2022-12-04 07:12:40 +0530

    chore : driverIndexLogic

--- echo-python/echo.py
+++ echo-python/echo.py
@@ -13,6 +13,7 @@
 from os import environ
 import logging
 import requests
+import json
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
@@ -20,11 +21,38 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
+def hex2str(hex):
+    """
+    Decodes a hex string into a regular string
+    """
+    return bytes.fromhex(hex[2:]).decode("utf-8")
+
+def str2hex(str):
+    """
+    Encodes a string as a hex string
+    """
+    return "0x" + str.encode("utf-8").hex()
+
+
+def performDriverBehaviourIndexCalc(json):
+    """
+    Parses the sensor data to spit out a driverIndex
+    """
+    return 7.3
+
+
 def handle_advance(data):
-    logger.info(f"Received advance request data {data}")
+    input = hex2str(data["payload"])
+    logger.info(f"Received input: {input}")
+
+    inputJSON = json.loads(input)
+    driverIndexCalculated = performDriverBehaviourIndexCalc(inputJSON)
+
+    driverIndexHex = str2hex(driverIndexCalculated)
+    
     logger.info("Adding notice")
-    notice = {"payload": data["payload"]}
-    response = requests.post(rollup_server + "/notice", json=notice)
+    # notice = {"payload": data["payload"]}
+    response = requests.post(rollup_server + "/notice", json=str2hex(str(driverIndexHex)))
     logger.info(f"Received notice status {response.status_code} body {response.content}")
     return "accept"
 
